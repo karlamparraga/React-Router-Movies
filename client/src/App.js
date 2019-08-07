de756c366537f4ca9067486e17b956d8c7d2d383
karlie0 <kamabrapa88@gmail.com>
2019-08-07 19:40:20 -0400

    Finished part 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
+import {BrowserRouter as Route} from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -15,7 +17,7 @@ const App = () => {
       <SavedList list={savedList} />
       <div>
         <Route path="/" component={MovieList}></Route>
-        <Route path="/movies:id" component={Movie}></Route>
+        <Route path="/movies/:id" component={Movie}></Route>
       </div>
     </div>
   );
